63d315a54c0fb27e7679b7580090db45bec7f6d3
Textspot <sachinkatarki1995@gmail.com>
2021-11-01 08:58:27 +0530

    Added:Welcome to snake and ladder

---
+++ src/com/bridgelab/snl/Snake_ladder.java
@@ -0,0 +1,12 @@
+package com.bridgelab.snl;
+
+public class Snake_ladder {
+
+	public static void main(String[] args) {
+		int Position = 0;
+		System.out.println("Welcome into Snake and Ladder program !!");
+		System.out.println("Your Starting Position is : " + Position);
+
+	}
+
+}

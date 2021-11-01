69bf2892e1e7817b648f480797838dcdad450c12
Textspot <sachinkatarki1995@gmail.com>
2021-11-01 09:49:23 +0530

    Added:dice play number

--- src/com/bridgelab/snl/Snake_ladder.java
+++ src/com/bridgelab/snl/Snake_ladder.java
@@ -4,22 +4,24 @@ public class Snake_ladder {
 
 	public static void main(String[] args) {
 		int position = 0;
+		int diceRollCount = 0;
 		System.out.println("Welcome into Snake and Ladder program !! ");
 		System.out.println("position is " + position);
 		int diceValue = (int) (Math.random() * 10) % 6 + 1;
 		Utility utility = new Utility();
-		utility.playOption(position, diceValue);
+		utility.playOption(position, diceValue, diceRollCount);
 
 	}
 }
 
 class Utility {
 
-	public void playOption(int position, int diceValue) {
+	public void playOption(int position, int diceValue, int diceRollCount) {
 
 		while (position <= 99) {
 
 			int optionValue = (int) (Math.random() * 10) % 3 + 1;
+			diceRollCount++;
 
 			switch (optionValue) {
 			case 1:
@@ -44,8 +46,10 @@ class Utility {
 				System.out.println("Your Position after snake is" + position);
 				break;
 			}
-			}
 		}
+		System.out.println("dice play number:: " + diceRollCount);
 	}
+
+}
 
 
